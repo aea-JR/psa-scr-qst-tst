080e76c712cbf87ff54e5e78043ae2cb20db4df9
Amier El Ahmad <amier.elahmad@justrelate.com>
2025-02-02 18:50:14 +0100

    Remove unused

--- src/Widgets/DropdownQuestionWidget/DropdownQuestionWidgetClass.ts
+++ src/Widgets/DropdownQuestionWidget/DropdownQuestionWidgetClass.ts
@@ -7,7 +7,7 @@ export const DropdownQuestionWidget = provideWidgetClass(
     attributes: {
       ...defaultQuestionAttributes,
       emptyOption: "boolean",
-      type: ["enum", { values: ["string_dropdown", "company_dropdown"] }],
+      type: ["enum", { values: ["string_dropdown"] }],
       options: ["widgetlist", { only: ["AnswerOptionWidget"] }],
       //options2: ["referencelist", { only: ["AnswerOption"] }]
 
