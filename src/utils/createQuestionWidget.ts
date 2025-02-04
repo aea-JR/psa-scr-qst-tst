adbb0c430dd5ea8a47c6981be5781726a05ed204
Amier El Ahmad <amier.elahmad@justrelate.com>
2025-02-04 14:18:48 +0100

    question widget creator

---
+++ src/utils/createQuestionWidget.ts
@@ -0,0 +1,25 @@
+import { Widget } from "scrivito";
+import { Question } from "../types/questionnaire";
+import { DropdownQuestionWidget } from "../Widgets/DropdownQuestionWidget/DropdownQuestionWidgetClass";
+import { InputQuestionWidget } from "../Widgets/InputQuestionWidget/InputQuestionWidgetClass";
+
+/**
+ * NOT USED
+ * INCOMPLETE
+ * @param question 
+ * @param questionId 
+ * @returns 
+ */
+export const createNewQuestionWidget = (question: Question, questionId: string): Widget => {
+	switch (question.type) {
+		case "string_single_line":
+		case "string_multi_line":
+		case "integer":
+		case "date":
+			return new InputQuestionWidget({ ...question, questionId: questionId });
+		case "string_dropdown":
+			return new DropdownQuestionWidget({ ...question, questionId: questionId, options: [] });
+		default:
+			throw "Unable to create new question widget. Invalid type!";
+	}
+}
