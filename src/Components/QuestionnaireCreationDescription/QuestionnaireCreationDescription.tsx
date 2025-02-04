f7233c3459a992942ddb52ea6b5ece4f8d840e8a
Amier El Ahmad <amier.elahmad@justrelate.com>
2025-02-04 14:10:06 +0100

    Improve descriptions

--- src/Components/QuestionnaireCreationDescription/QuestionnaireCreationDescription.tsx
+++ src/Components/QuestionnaireCreationDescription/QuestionnaireCreationDescription.tsx
@@ -1,37 +1,40 @@
 import * as React from "react";
 import "./QuestionnaireCreationDescription.scss";
+import { QuestionnaireStatus } from "../../types/questionnaire";
 
 
 
 interface DescriptionProps {
 	isValid: boolean;
-	isCreating: boolean
+	status: QuestionnaireStatus;
 }
 //TODO: refactor
-export const Description: React.FC<DescriptionProps> = ({ isValid, isCreating }) => {
-	let message = null;
+export const Description: React.FC<DescriptionProps> = ({ isValid, status }) => {
 
-	if (isCreating) {
-		message = (
-			<>
-				<span
-					className="spinner-border spinner-border-sm"
-					role="status"
-					aria-hidden="true"
-				></span>
-				<span className="message"> Creating the questionnaire on PisaSales side, please wait...</span>
-			</>
-		);
-	} else if (!isValid) {
-		message = (
-			<span className="message">
-				Questionnaire has invalid attributes, please check and fix the issues
-				before creating.
-			</span>
-		);
-	} else {
-		message = <span className="message">Creates a questionnaire in PisaSales</span>;
-	}
-
-	return <div className="description notice-body">{message}</div>;
+	const getMessage = (): string => {
+		switch (status) {
+			case "creationPending":
+				return "This questionnaire has not been created in PisaSales yet.";
+			case "inCreation":
+				return "Creating the questionnaire in PisaSales. Please wait...";
+			case "invalid":
+				return "This questionnaire contains invalid attributes. Please review and correct them.";
+			case "pendingUpdate":
+				return "Unsaved changes detected. Push these updates to PisaSales to keep your questionnaire up to date.";
+			case "updating":
+				return "Updating questionnaire data. Please wait...";
+			default:
+				return "The questionnaire is ready for use.";
+		}
+	};
+	return <div className="description notice-body">
+		{status == "inCreation" && <span
+			className="spinner-border spinner-border-sm"
+			role="status"
+			aria-hidden="true"
+		></span>}
+		<span className="message">
+			{getMessage()}
+		</span>
+	</div>;
 };
--- src/Components/QuestionnaireMessageBlock/QuestionnaireMessageBlock.tsx
+++ src/Components/QuestionnaireMessageBlock/QuestionnaireMessageBlock.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './QuestionnaireMessageBlock.scss';
 
+const WARNING = "Warning:";
+const EDITORS_INFO = "Editor's info:";
 const messages = {
-	creationPending: { prefix: "Editor's info:", content: "This questionnaire has not been created yet in PisaSales!" },
-	invalidAttributes: { prefix: "Editor's info:", content: "Some attributes are invalid!" },
-	warningCreationPending: { prefix: "Warning:", content: "This questionnaire has not been created yet in PisaSales!" },
-	pisaOffline: { prefix: "Warning:", content: "PisaSales is not reachable. Please check your configuration or contact your administrator." },
-	updatePending: { prefix: "Editor's info:", content: "There are unsyned changes. Please update the questionnaire or revert these changes from the properties tab." }
+	creationPending: { prefix: EDITORS_INFO, content: "This questionnaire has not been created in PisaSales yet." },
+	invalidAttributes: { prefix: EDITORS_INFO, content: "Some attributes are invalid. Please review and correct them." },
+	warningCreationPending: { prefix: WARNING, content: "This questionnaire is missing in PisaSales. Ensure it has been created before using it." },
+	pisaOffline: { prefix: WARNING, content: "PisaSales is not reachable. Please check your configuration or contact your administrator." },
+	updatePending: { prefix: EDITORS_INFO, content: "This questionnaire has unsaved local changes. To ensure all modifications are reflected in PisaSales, please push the updates from the properties tab." }
 };
 
 interface Props {
@@ -20,7 +22,7 @@ export const QuestionnaireMessageBlock: React.FC<Props> = ({ type, className }) 
 
 	return (
 		<div className="qst-message-block-container">
-			<div className={`message-block alert fade show ${type == "creationPending" ? "alert-warning" : "alert-error"} ${className || ''}`}>
+			<div className={`message-block alert fade show ${message.prefix == EDITORS_INFO ? "alert-warning" : "alert-error"} ${className || ''}`}>
 				<p>
 					<strong>{message.prefix}</strong> {message.content}
 				</p>
