f825991baadfc6893c5d3806bdc124c8690a84a8
Amier El Ahmad <amier.elahmad@justrelate.com>
2025-02-07 20:24:21 +0100

    proper naming

--- src/Widgets/QuestionnaireContainerWidget/useEditModeSync.ts
+++ src/Widgets/QuestionnaireContainerWidget/useEditModeSync.ts
@@ -4,18 +4,18 @@ import { extractQuestionsAndOptions } from "../../utils/extractQuestionsAndOptio
 import { each } from "lodash-es";
 import { useDataItem } from "scrivito";
 
-export const useEditModeSync = (widget: Widget) => {
+export const useEditModeSync = (qstMainWidget: Widget) => {
 	const isEditing = isInPlaceEditingActive();
-	const steps = widget.get("steps") as Widget[];
+	const steps = qstMainWidget.get("steps") as Widget[];
 	const stepsLength = steps.length;
-	const { questionWidgets } = extractQuestionsAndOptions(widget);
+	const { questionWidgets } = extractQuestionsAndOptions(qstMainWidget);
 	const dataItem = useDataItem();
-	const projectIdSource = widget.get("projectIdSource") as string;
-	const contactIdSource = widget.get("contactIdSource") as string;
-	const activityIdSource = widget.get("activityIdSource") as string;
-	const projectIdDataItemField = widget.get("projectIdDataItemField") as string;
-	const contactIdDataItemField = widget.get("contactIdDataItemField") as string;
-	const activityIdDataItemField = widget.get("activityIdDataItemField") as string;
+	const projectIdSource = qstMainWidget.get("projectIdSource") as string;
+	const contactIdSource = qstMainWidget.get("contactIdSource") as string;
+	const activityIdSource = qstMainWidget.get("activityIdSource") as string;
+	const projectIdDataItemField = qstMainWidget.get("projectIdDataItemField") as string;
+	const contactIdDataItemField = qstMainWidget.get("contactIdDataItemField") as string;
+	const activityIdDataItemField = qstMainWidget.get("activityIdDataItemField") as string;
 
 	// Sync Question Positions
 	useEffect(() => {
@@ -32,7 +32,7 @@ export const useEditModeSync = (widget: Widget) => {
 	// Sync Step Attributes & Form Type
 	useEffect(() => {
 		if (!isEditing) { return; }
-		const steps = widget.get("steps") as Widget[];
+		const steps = qstMainWidget.get("steps") as Widget[];
 		steps.forEach((step, i) => {
 			step.update({
 				stepNumber: i + 1,
@@ -40,7 +40,7 @@ export const useEditModeSync = (widget: Widget) => {
 			});
 		});
 
-		widget.update({
+		qstMainWidget.update({
 			formType: stepsLength > 1 ? "multi-step" : "single-step",
 		});
 	}, [steps]);
@@ -51,7 +51,7 @@ export const useEditModeSync = (widget: Widget) => {
 		const updatePreview = (source: string, field: string, key: string) => {
 			if (source === "data-item") {
 				const value = dataItem?.get(field) as string || "";
-				widget.update({ [key]: value });
+				qstMainWidget.update({ [key]: value });
 			}
 		};
 
