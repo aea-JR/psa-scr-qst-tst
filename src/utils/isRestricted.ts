08f13f03b7454f594b308d0d199816385c6e15f8
Amier El Ahmad <amier.elahmad@justrelate.com>
2025-02-11 11:50:32 +0100

    Reduce complexity for restriction check

--- src/utils/isRestricted.ts
+++ src/utils/isRestricted.ts
@@ -7,19 +7,5 @@ import { Obj, Widget } from "scrivito"
  * @returns `true`, if usage is restricted for the current site, otherwise `false`.
  */
 export const isUsageRestricted = (qstMainWidget: Widget): boolean => {
-	let currentContainer = qstMainWidget.container();
-
-	while (currentContainer) {
-		if (currentContainer instanceof Obj) {
-			return !currentContainer.isRestricted();
-		}
-		if (currentContainer instanceof Widget) {
-			currentContainer = currentContainer.container();
-		} else {
-			console.warn("Unexpected structure: Widget is not part of a valid Obj.");
-			break;
-		}
-	}
-	console.error("Widget is not placed on any site.");
-	return false;
+	return !qstMainWidget.obj().isRestricted();
 };
