85c349cc750b3d55daac9c14c196eff4c68d1895
Amier El Ahmad <amier.elahmad@justrelate.com>
2025-02-18 10:34:03 +0100

    Improve step preview

--- src/Widgets/QuestionnaireStepWidget/QuestionnaireStepWidgetComponent.tsx
+++ src/Widgets/QuestionnaireStepWidget/QuestionnaireStepWidgetComponent.tsx
@@ -2,17 +2,19 @@ import { ContentTag, isInPlaceEditingActive, provideComponent } from "scrivito";
 import { QuestionnaireStepWidget } from "./QuestionnaireStepWidgetClass";
 import "./QuestionnaireStepWidget.scss";
 import { useQuestionnaireStepsContext } from "../../contexts/QuestionnaireStepsContext";
+import { useDynamicBackground } from "../../hooks/useDynamicBackground";
 
 provideComponent(QuestionnaireStepWidget, ({ widget }) => {
   const { getStepInfo } = useQuestionnaireStepsContext();
   const data = getStepInfo(widget.id());
   const isMultiStepsWithActiveEditing = isInPlaceEditingActive() && !data.isSingleStep;
+  const titleBgColor = useDynamicBackground(".step-preview-count")
   //TODO: improve or remove step preview
   return (
     <>
-      {/* {isMultiStepsWithActiveEditing && (
-        <span className="step-preview-count">{"Step " + data.stepNumber}</span>
-      )} */}
+      {isMultiStepsWithActiveEditing && (
+        <span className="step-preview-count" style={{ backgroundColor: titleBgColor || "transparent" }}>{"Step " + data.stepNumber}</span>
+      )}
       <div
         className={`step-container  ${isMultiStepsWithActiveEditing ? "step-border" : data.isActive || data.isSingleStep ? "" : "hide"}`}
         data-step-number={data.stepNumber}
