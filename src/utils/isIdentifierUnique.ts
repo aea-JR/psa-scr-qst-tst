96680cfe3a8854a5e9b486a0732cd9926e5a471f
Amier El Ahmad <amier.elahmad@justrelate.com>
2025-02-19 16:55:24 +0100

    Fix idn check not working after steps introduction

--- src/utils/isIdentifierUnique.ts
+++ src/utils/isIdentifierUnique.ts
@@ -2,30 +2,40 @@ import { Widget } from "scrivito";
 import { isEmpty } from "lodash-es";
 import { getIdentifier } from "./getIdentifier";
 import { getQuestionnaireContainerWidget } from "./getQuestionnaireContainerWidget";
-import { CONTENT } from "../constants/constants";
+import { CONTENT, STEPS } from "../constants/constants";
 
 export function isIdentifierUnique(widget: Widget, type: string) {
   const identifier = getIdentifier(widget);
   if (isEmpty(identifier)) {
     return true;
   }
+  return type === "AnswerOptionWidget"
+    ? isOptionIdentifierUnique(identifier, widget)
+    : isQuestionIdentifierUnique(identifier, widget);
+}
 
-  const container =
-    type == "AnswerOptionWidget"
-      ? widget.container()
-      : getQuestionnaireContainerWidget(widget);
+const isOptionIdentifierUnique = (identifier: string, widget: Widget) => {
+  const container = widget.container();
   if (!container) {
     return true;
   }
-
-  const otherWidget = (
-    type == "AnswerOptionWidget"
-      ? container.widgets()
-      : (container.get(CONTENT) as Widget[])
-  ).find(
-    (child) =>
-      getIdentifier(child) === identifier && child.id() !== widget.id(),
+  return !container.widgets().some(
+    (child) => getIdentifier(child) === identifier && child.id() !== widget.id()
   );
+};
 
-  return !otherWidget;
-}
+const isQuestionIdentifierUnique = (identifier: string, widget: Widget) => {
+  const container = getQuestionnaireContainerWidget(widget);
+  if (!container) {
+    return true;
+  }
+  const steps = container.get(STEPS) as Widget[];
+  if (isEmpty(steps)) {
+    return true;
+  }
+  const questions = steps.flatMap((step) => step.get(CONTENT) as Widget[]);
+
+  return !questions.some(
+    (child) => getIdentifier(child) === identifier && child.id() !== widget.id()
+  );
+};
