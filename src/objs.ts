f120ffe131e7398e2dfc1fc69850b12b3d1ca5e6
Amier El Ahmad <amier.elahmad@justrelate.com>
2025-02-20 15:51:31 +0100

    Remove unused

--- src/objs.ts
+++
@@ -1,5 +0,0 @@
-// export function loadQuestionnaireObjs(): void {
-// 	import.meta.glob(['./Objs/**/*EditingConfig.ts', './Objs/**/*EditingConfig.tsx', './Objs/**/*ObjClass.ts', './Objs/**/*ObjClass.tsx'], {
-// 		eager: true,
-// 	});
-// }
